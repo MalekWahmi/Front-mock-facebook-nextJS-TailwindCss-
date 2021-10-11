9ce2dcc4f3f3dedce44c762310e3d02bf28309cc
MalekWahmi <wahmimalek@gmail.com>
2021-10-11 19:48:00 +0100

    header

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -1,14 +1,59 @@
-import React from 'react'
-import Image from "next/image"
+import React from "react";
+import Image from "next/image";
+import {
+  BeakerIcon,
+  HomeIcon,
+  FlagIcon,
+  PlayIcon,
+  ShoppingCartIcon,
+  UserGroupIcon,
+  ViewGridIcon,
+  ChevronDownIcon,
+  BellIcon,
+  ChatIcon,
+} from "@heroicons/react/solid";
+import { SearchIcon  } from "@heroicons/react/outline";
+import HeaderIcon from "./HeaderIcon";
 
 export default function Header() {
-    return (
-        <div>
-            <h1></h1>
-            <div>
-                {/* <Image/> */}
-            </div>
+  return (
+    <div className="sticky top-0 z-50 bgèwhite flex items-center p-2 lg:px-5 shadow-md">
+      <div className="flex items-center">
+        <Image
+          src="https://links.papareact.com/5me"
+          width={40}
+          height={40}
+          layout="fixed"
+        />
+        <div className="flex ml-2 items-center rounded-full bg-gray-100 p-2">
+          <SearchIcon className="h-6 text-gray-600" />
+          <input
+            className=" hidden md:inline-flex ml-2 items-center bg-transparent outline-none placeholder-gray-500 flex-shrink"
+            type="text"
+            placeholder="Search Facebook"
+          />
         </div>
-    
-    )
+        {/* Center */}
+        <div className="flex justify-center flex-grow">
+          <div className="flex space-x-6 md:space-x-2">
+            <HeaderIcon active Icon={HomeIcon} />
+            <HeaderIcon Icon={FlagIcon} />
+            <HeaderIcon Icon={PlayIcon} />
+            <HeaderIcon Icon={ShoppingCartIcon} />
+            <HeaderIcon Icon={UserGroupIcon} />
+          </div>
+        </div>
+        {/* Rigth */}
+        <div className="flex items-center sm:space-x-2 justify-end">
+          {/* Profile pic */}
+
+          <p className="whitespace font-semibold pr-3">Malek Wahmi</p>
+          <ViewGridIcon className="icon" />
+          <ChatIcon className="icon" />
+          <BellIcon className="icon" />
+          <ChevronDownIcon className="icon" />
+        </div>
+      </div>
+    </div>
+  );
 }
