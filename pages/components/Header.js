90db5a9d9a29b74a9cd560244f6aec916b27b3f0
MalekWahmi <wahmimalek@gmail.com>
2021-10-11 17:46:25 +0100

    init

---
+++ pages/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Image from "next/image"
+
+export default function Header() {
+    return (
+        <div>
+            <h1></h1>
+            <div>
+                {/* <Image/> */}
+            </div>
+        </div>
+    
+    )
+}
---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from "next/head";
+import Header from "./components/Header"
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>My Social Media</title>
+      </Head>
+      <Header/>
+       <main>
+
+       </main>
+    </div>
+  );
+}
